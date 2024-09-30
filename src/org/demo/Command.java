19137c033e712fe007b7887677bddd45739892ff
shankarranganathan <shankarmech100@gmail.com>
2024-09-30 07:01:56 +0530

    shancommit

---
+++ src/org/demo/Command.java
@@ -0,0 +1,11 @@
+package org.demo;
+
+public class  Command {
+	public static void main(String[] args) {
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+		
+	}
+
+}
